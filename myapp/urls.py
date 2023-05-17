14268c3bd913559f68d01a56aea1e3d660272af6
dharshini-jp <jayapriyadharshini02@gmail.com>
2023-05-17 13:06:23 +0530

    webpage

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chat, name='chat')
+    path('about/',views.about,name='about'),
+    path('chat/', views.chat, name='chat'),
+    path('contact/', views.contact, name='contact'),
+    path('contactus/', views.contactus, name='contactus'),
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('services/', views.services, name='services'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -8,4 +8,25 @@ def chat(request):
         response = requests.post('http://localhost:5005/webhooks/rest/webhook', json={'message': message})
         print('Received response:', response.json())
 
-    return render(request, 'index.html')
+    return render(request, 'chat.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def contactus(request):
+    return render(request,'contactus.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def services(request):
+    return render(request, 'services.html')
